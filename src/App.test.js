cb458312f690fb1dcb622222469ad524fd6f6170
giuliogallerini <giulio.gallerini.work.com>
2016-11-01 11:32:25 +0100

    add basic style

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme'
 import chai, { expect } from 'chai'
 import chaiEnzyme from 'chai-enzyme'
 import App from './App'
+import Title from './components/Title'
 
 chai.use(chaiEnzyme())
 
@@ -15,5 +16,13 @@ describe('<App />', () => {
 
   it('says Hello World', () => {
     expect(app).to.have.text('Hello World!')
+  })
+
+  it('has a App class', () =>{
+    expect(app).to.have.className('app')
+  })
+
+  it('renders the Title', () => {
+    expect(app).to.have.descendants(Title)
   })
 })
